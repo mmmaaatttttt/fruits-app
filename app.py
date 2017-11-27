2beeaa15aed9bf7d89fb51cb53a4b765ddbca3d9
Matt Lane <lane.matthew@gmail.com>
2017-11-27 12:08:08 -0800

    adding decorator syntax

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 from fruits import Fruit
 from flask_modus import Modus
 
@@ -10,6 +10,23 @@ fruits = [
 
 app = Flask(__name__)
 modus = Modus(app)
+
+from functools import wraps
+
+def ensure_valid_id(fn):
+    @wraps(fn)
+    # these are args and kwargs from function
+    # being decorated: show, edit, etc
+    def wrapper(*args, **kwargs):
+        found_fruit = [
+            fruit
+            for fruit in fruits
+            if fruit.id == kwargs.get('id')
+        ]
+        if len(found_fruit) == 1:
+            return fn(*args, **kwargs)
+        return abort(404)
+    return wrapper
 
 @app.route("/fruits", methods=["GET", "POST"])
 def index():
@@ -27,31 +44,31 @@ def new():
     return render_template("new.html")
 
 @app.route("/fruits/<int:id>", methods=["GET", "DELETE", "PATCH"])
+@ensure_valid_id
 def show(id):
     found_fruit = [
         fruit
         for fruit in fruits 
         if fruit.id == id
-    ]
+    ][0]
     if request.method == b"DELETE":
-        fruits.remove(found_fruit[0])
+        fruits.remove(found_fruit)
     if request.method == b"PATCH":
-        found_fruit[0].name = request.form['name']
-        found_fruit[0].sweetness = int(request.form['sweetness'])
-    if found_fruit and request.method == "GET":
-        return render_template("show.html", fruit=found_fruit[0])
+        found_fruit.name = request.form['name']
+        found_fruit.sweetness = int(request.form['sweetness'])
+    if request.method == "GET":
+        return render_template("show.html", fruit=found_fruit)
     return redirect(url_for("index"))
 
 @app.route("/fruits/<int:id>/edit")
+@ensure_valid_id
 def edit(id):
     found_fruit = [
         fruit
         for fruit in fruits 
         if fruit.id == id
-    ]
-    if found_fruit:
-        return render_template("edit.html", fruit=found_fruit[0])
-    return redirect(url_for("index"))
+    ][0]
+    return render_template("edit.html", fruit=found_fruit)
 
 if __name__ == "__main__":
     app.run(debug=True)
